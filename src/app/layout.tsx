cbdcfe89a3b5be63be6dc37f459e6f88145ae8ff
HiddenIpex <leeleonard609@gmail.com>
2025-08-11 02:02:06 +0100

    Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -40,6 +41,7 @@ export default function RootLayout({
       <body
         className={`${montserrat.variable} antialiased`}
       >
+        <Analytics />
         {children}
       </body>
     </html>
