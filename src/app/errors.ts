528b427f8ef1586d89edcd245def4d3aa5052d57
HiddenIpex <leeleonard609@gmail.com>
2025-08-11 01:29:06 +0100

    Update errors.ts

--- src/app/errors.ts
+++ src/app/errors.ts
@@ -4,7 +4,7 @@ export enum errorTypes {
     SIGNUP_FAILED = "SIGNUP_FAILED"
 }
 
-export const errorMessages: any = {
+export const errorMessages = {
     [errorTypes.INVALID_EMAIL]: "Please enter a valid email address.",
     [errorTypes.EMAIL_ALREADY_EXISTS]: "This email is already registered.",
     [errorTypes.SIGNUP_FAILED]: "Sign up failed. Please try again later."
